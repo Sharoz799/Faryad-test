7d2aa64c9e3d2346c320207a68c948c4562ad290
Sharoz799 <sharoz.inocent@gmail.com>
2020-03-31 18:59:51 +0500

    update upto 31.03.2021

---
+++ Static/js/sign.js
@@ -0,0 +1,94 @@
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const phone = document.getElementById('Phone');
+const org = document.getElementById('Org');
+const add = document.getElementById('address');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    const phoneValue = phone.value.trim();
+    const orgValue = org.value.trim();
+    const addValue = add.value.trim();
+	
+	if(orgValue === '') {
+		setErrorFor(org, 'Oraganization name cannot be blank');
+	} else {
+		setSuccessFor(org);
+    }
+
+    if(addValue === '') {
+		setErrorFor(add, 'Address cannot be blank');
+	} else {
+		setSuccessFor(add);
+    }
+    
+    if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+    }
+    if(phoneValue === '') {
+		setErrorFor(phone, 'Phone cannot be blank');
+	} else if (!isPhone(phoneValue)) {
+		setErrorFor(phone, 'Not a valid phone no');
+	} else {
+		setSuccessFor(phone);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Confirm password cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function isPhone(phone) {
+    return /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+    .test(phone);
+}
